a6469bc52be1566814641979352654791e719f0a
deepika186 <76603313+deepika186@users.noreply.github.com>
2020-12-27 20:26:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function changecolor() {
+    document.getElementById("date").style.color = "blue";
+};
+
+function flipimage() {
+document.getElementById("image1").src = "https://images.unsplash.com/photo-1502808895237-8d699559bd4c?crop=entropy&cs=srgb&fm=jpg&ixid=MXwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHw&ixlib=rb-1.2.1&q=85"
+};
+
+function settheDate() {
+    document.getElementById("dates").innerHTML = new getDate();
+};
+
