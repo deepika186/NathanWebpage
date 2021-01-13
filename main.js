bddf0ac9527cd9174072c44e90c3cad57d665538
deepika186 <76603313+deepika186@users.noreply.github.com>
2021-01-13 07:55:48 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ function changecolor() {
 };
 
 function flipimage() {
+    document.getElementById("audio_file").play();
 document.getElementById("image1").src = "https://images.unsplash.com/photo-1502808895237-8d699559bd4c?crop=entropy&cs=srgb&fm=jpg&ixid=MXwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHw&ixlib=rb-1.2.1&q=85"
 };
 
